d3533954e1bd08d342c525787b049aa40ac1965b
LCHM228 <71660110+LCHM228@users.noreply.github.com>
2020-11-26 19:59:11 +0500

    AccountController

---
+++ Models/EmployeeCost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _66bitProject.Models
+{
+    public class EmployeeCost
+    {
+        public User EmployeeId { get; set; }
+        public Costs CostId { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace _66bitProject.Models
+{
+    public class User : IdentityUser<int>
+    {
+        #region Поля профиля
+
+        public string FirstName { get; set; }
+        public int Year { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Position { get; set; }
+
+        public Department DepartmentID { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Список ролей пользователя
+        /// </summary>
+        public virtual ICollection<IdentityRole<int>> Roles { get; set; } = new List<IdentityRole<int>>();
+
+        /// <summary>
+        /// Список разрешений пользователя
+        /// </summary>
+        public virtual ICollection<IdentityUserClaim<int>> UserClaims { get; set; } = new List<IdentityUserClaim<int>>();
+
+
+
+    }
+}
